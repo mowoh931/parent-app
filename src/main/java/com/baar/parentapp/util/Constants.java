6275db7eaeebc9e9fbecedf249f21b57c50b9c92
Mowoh931 <mowohbaar@yahoo.com>
2023-12-06 21:53:34 -0600

    Made some changes for Constants class

--- src/main/java/com/baar/parentapp/util/Constants.java
+++ src/main/java/com/baar/parentapp/util/Constants.java
@@ -3,7 +3,6 @@ package com.baar.parentapp.util;
 public interface Constants {
     String studentPathTxt = "src/main/data/student.txt";
     String studentPath = "src/main/data/student.json";
-
     String users = "F:\\IntelliJ Idea Projects\\MODULAR\\parent-app\\src\\main\\resources\\user.json";
-   String POST_URL ="https://jsonplaceholder.typicode.com";
+    String POST_URL = "https://jsonplaceholder.typicode.com";
 }

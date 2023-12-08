2477461daca7ef7ff7a1e199338a26a63d71573c
JayMgitM <mowohbaar@yahoo.com>
2023-12-08 17:52:36 -0600

    comment to commit to ls_new

--- src/main/java/com/baar/parentapp/util/Constants.java
+++ src/main/java/com/baar/parentapp/util/Constants.java
@@ -6,4 +6,5 @@ public interface Constants {
     String users = "F:\\IntelliJ Idea Projects\\MODULAR\\parent-app\\src\\main\\resources\\user.json";
     String POST_URL = "https://jsonplaceholder.typicode.com/posts";
     // send ls  to B-256
+    // Commit to ls_new
 }

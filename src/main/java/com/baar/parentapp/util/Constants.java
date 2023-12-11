9c3309b62a913830d1be4967d087689b627c94b1
JayMgitM <mowohbaar@yahoo.com>
2023-12-11 10:27:35 -0600

    Committing

--- src/main/java/com/baar/parentapp/util/Constants.java
+++ src/main/java/com/baar/parentapp/util/Constants.java
@@ -1,10 +1,11 @@
 package com.baar.parentapp.util;
 
 public interface Constants {
-    String studentPathTxt = "src/main/data/student.txt";
-    String studentPath = "src/main/data/student.json";
-    String users = "F:\\IntelliJ Idea Projects\\MODULAR\\parent-app\\src\\main\\resources\\user.json";
-    String POST_URL = "https://jsonplaceholder.typicode.com/posts";
-    // send ls  to B-256
-    // Commit to ls_new
+  String studentPathTxt = "src/main/data/student.txt";
+  String studentPath = "src/main/data/student.json";
+  String users = "F:\\IntelliJ Idea Projects\\MODULAR\\parent-app\\src\\main\\resources\\user.json";
+  String POST_URL = "https://jsonplaceholder.typicode.com/posts";
+  // send ls  to B-256
+  // Commit to ls_new
+  // Commit to B-256
 }

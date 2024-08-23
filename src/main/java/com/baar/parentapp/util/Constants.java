8b593170a929c1d105b8f49515852dad4d8a484d
JayMgitM <mowohbaar@yahoo.com>
2024-08-23 16:35:29 -0500

    data source update

--- src/main/java/com/baar/parentapp/util/Constants.java
+++ src/main/java/com/baar/parentapp/util/Constants.java
@@ -3,10 +3,6 @@ package com.baar.parentapp.util;
 public interface Constants {
   String studentPathTxt = "src/main/data/student.txt";
   String studentPath = "src/main/data/student.json";
-  String users = "F:\\IntelliJ Idea Projects\\MODULAR\\parent-app\\src\\main\\resources\\user.json";
+  String users = "src/main/resources/user.json";
   String POST_URL = "https://jsonplaceholder.typicode.com/posts";
-  // send ls  to B-256
-  // Commit to ls_new
-  // Commit to B-257
-  // commit to ls.
 }
